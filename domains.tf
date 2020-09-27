0c9ff1abe60dbfaaa590c86f88b4f47207bd15fa
Matei-Alexandru Gardus <matei@gard.us>
2020-09-26 22:19:45 -0700

    Import DNS & instance state from AWS to Terraform
    Include all pertinent information with regards to domains
    and instances deployed under AWS in README, and include
    relevant Terraform files.
    
    Highly important to note that, in order to use the current
    state from AWS, one must run `terraform import` on all resources
    generated within these Terraform files to be up to date with the
    current state.
    
    .tfstate files will not be committed to the repository, as they
    include highly-sensitive information.
    
    Additionally, README now includes mentions and explanations for
    Terraform files. As of this commit, Terraform fully manages ALL
    AWS-provisioned resources.
    
    Pre-Tangle Terraform files
    
    Post-tangle Terraform files + Org-Mode stuff

---
+++ domains.tf
@@ -0,0 +1,224 @@
+resource "aws_route53_zone" "acmucsd-com-public" {
+  name    = "acmucsd.com"
+  comment = "ACM's main domain for stuff."
+
+  tags = {
+  }
+}
+
+resource "aws_route53_record" "acmucsd-com-A" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "acmucsd.com"
+  type    = "A"
+  records = ["104.198.14.52"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "www-acmucsd-com-A" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "www.acmucsd.com"
+  type    = "A"
+  records = ["104.198.14.52"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "acmucsd-com-NS" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "acmucsd.com"
+  type    = "NS"
+  records = ["ns-2024.awsdns-61.co.uk.", "ns-278.awsdns-34.com.", "ns-1200.awsdns-22.org.", "ns-591.awsdns-09.net."]
+  ttl     = "172800"
+}
+
+resource "aws_route53_record" "acmucsd-com-SOA" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "acmucsd.com"
+  type    = "SOA"
+  records = ["ns-2024.awsdns-61.co.uk. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
+  ttl     = "900"
+}
+
+resource "aws_route53_record" "s1-_domainkey-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "s1._domainkey.acmucsd.com"
+  type    = "CNAME"
+  records = ["s1.domainkey.u17821998.wl249.sendgrid.net"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "s2-_domainkey-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "s2._domainkey.acmucsd.com"
+  type    = "CNAME"
+  records = ["s2.domainkey.u17821998.wl249.sendgrid.net"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "ai-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "ai.acmucsd.com"
+  type    = "CNAME"
+  records = ["acmucsd-ai.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "sendgrid-ai-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "17821998.ai.acmucsd.com"
+  type    = "CNAME"
+  records = ["sendgrid.net"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "api-ai-acmucsd-com-A" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "api.ai.acmucsd.com"
+  type    = "A"
+  records = ["104.155.168.98"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "apitest-ai-acmucsd-com-A" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "apitest.ai.acmucsd.com"
+  type    = "A"
+  records = ["35.208.23.117"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "em4616-ai-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "em4616.ai.acmucsd.com"
+  type    = "CNAME"
+  records = ["u17821998.wl249.sendgrid.net"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "url4522-ai-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "url4522.ai.acmucsd.com"
+  type    = "CNAME"
+  records = ["sendgrid.net"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "api-test-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "api-test.acmucsd.com"
+  type    = "CNAME"
+  records = ["acmucsd-portal-testing.herokuapp.com"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "api-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "api.acmucsd.com"
+  type    = "CNAME"
+  records = ["shallow-koi-v9n1nho6ee48b480cn08m1hr.herokudns.com"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "design-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "design.acmucsd.com"
+  type    = "CNAME"
+  records = ["acmdesign.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "hack-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "hack.acmucsd.com"
+  type    = "CNAME"
+  records = ["acmhack-site.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "members-test-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "members-test.acmucsd.com"
+  type    = "CNAME"
+  records = ["members-nightly.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "members-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "members.acmucsd.com"
+  type    = "CNAME"
+  records = ["acmucsd.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "space2020-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "space2020.acmucsd.com"
+  type    = "CNAME"
+  records = ["space2020.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "splash-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "splash.acmucsd.com"
+  type    = "CNAME"
+  records = ["acmucsd-purple.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "static-template-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "static-template.acmucsd.com"
+  type    = "CNAME"
+  records = ["acm-static.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "tree-acmucsd-com-CNAME" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "tree.acmucsd.com"
+  type    = "CNAME"
+  records = ["acmucsd-tree.netlify.app"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "vote-acmucsd-com-A" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "vote.acmucsd.com"
+  type    = "A"
+  records = ["217.156.97.70"]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "bot-acmucsd-com-A" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "bot.acmucsd.com"
+  type    = "A"
+  records = [aws_instance.breadbot.public_ip]
+  ttl     = "3600"
+}
+
+resource "aws_route53_record" "pass-acmucsd-com-A" {
+  zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+  name    = "pass.acmucsd.com"
+  type    = "A"
+  records = [aws_instance.pass.public_ip]
+  ttl     = "3600"
+}
+
+# WIP
+# resource "aws_route53_record" "live-acmucsd-com-A" {
+#   zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+#   name    = "live.acmucsd.com"
+#   type    = "A"
+#   records = [aws_instance.live.public_ip]
+#   ttl     = "3600"
+# }
+#
+# resource "aws_route53_record" "mc-acmucsd-com-A" {
+#   zone_id = aws_route53_zone.acmucsd-com-public.zone_id
+#   name    = "mc.acmucsd.com"
+#   type    = "A"
+#   records = [aws_instance.minecraft.public_ip]
+#   ttl     = "3600"
+# }
--- instances.tf
+++ instances.tf
@@ -2,55 +2,116 @@ provider "aws" {
   region = "us-west-1"
 }
 
+resource "aws_key_pair" "stormfirefox1" {
+  key_name   = "stormfirefox1-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCj25h71sryk/5epo3yObh8HspR6ODMyaF/TSSC1SpIB2f9mPUKeCA8k32mrNpRCtVVwAhCn9OoVLfbfs4KzPNrxVByexqH6FORXY75mRUVdqF2qv9WDk7lANiTXV12+b9sMhtbGsU7r+CXGMCN+QZvnvLFb9xadKxNhJIyy+yQFu931uKl+GcGgePs4yj8uRLQ+o1yNU1WxzZgGHbBRAv/B98RCWoJTd+8RHfT/OxQL0PKx/LdHae/ium99xEJQ5DrbtMI+9BEM4vKN0FMCuVjPQ8sC8tIxzyqWZYyCIkiZmoH7TKGvD1Yg9jI9RxcuXB5793seiORAw9pH/U5w/8JKie5eGWXgrJkHMqqrvNGrw6TEk2gfaBllm8QSfS8O97xs6/BC1ENXUZ9R4rEGKpW9+pzHx/FTyD6FMMRstRcPjPCtU6F15g6XYAAjpbvxn4RiLT1Rk3mbJtM2xTAIUjYSfukGKzAs5gRp3TSNgdN+nsmcbJRMsA8FXubogDtYUO3VbpjsALA/bBHtjKziBVhbnoQ6PrXFrFaojeSq4Sf8iOOXK29Xfv/TSc+potmfw4wQMwSXJgOBFFhv9Xa/LwGf+1KpHpsfHO4xJ2yLpTUoiryvsbDsNk0+JbkMUgfNrJ+W01Lwf4qMKXbBOtNFf8gkTnj2Af3N6OCFa8XdgRO+Q== STORM_GPG_2FB5275E"
+}
+
+resource "aws_security_group" "allow_https_ssh" {
+  name        = "allow_https_ssh"
+  description = "Allow HTTPS/SSH traffic"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Ubuntu 20.04 LTS instance, for good measure
 # Find other AMI's at https://cloud-images.ubuntu.com/locator/ec2/
 resource "aws_instance" "breadbot" {
-  ami           = "ami-0cd230f950c3de5d8"
-  instance_type = "t3a.nano"
-  tags {
+  ami             = "ami-021809d9177640a20"
+  instance_type   = "t3a.nano"
+  key_name        = aws_key_pair.stormfirefox1.key_name
+  security_groups = [aws_security_group.allow_https_ssh.name]
+  tags = {
     Name = "BreadBot"
   }
 }
 
 resource "aws_instance" "pass" {
-  ami           = "ami-0cd230f950c3de5d8"
-  instance_type = "t3a.nano"
-  tags {
+  ami             = "ami-021809d9177640a20"
+  instance_type   = "t3a.nano"
+  key_name        = aws_key_pair.stormfirefox1.key_name
+  security_groups = [aws_security_group.allow_https_ssh.name]
+  tags = {
     Name = "Password Manager"
   }
 }
 
 # ACM Live runs on Ubuntu 16.04 LTS
 # We'll use a HVM instance of it, no extra stuff on top.
-resource "aws_instance" "live" {
-  ami           = "ami-0a63cd87767e10ed4"
-  instance_type = "m5a.large"
-  tags {
-    Name = "ACM Live"
-  }
+# resource "aws_instance" "live" {
+#   ami           = "ami-0a63cd87767e10ed4"
+#   instance_type = "m5a.large"
+#   key_name        = aws_key_pair.stormfirefox1.key_name
+#   security_groups = [aws_security_group.allow_https_ssh.name]
+#   tags {
+#     Name = "ACM Live"
+#   }
+# }
+
+# RETIRED TEMPORARILY
+# resource "aws_instance" "minecraft" {
+#   ami           = "ami-021809d9177640a20"
+#   instance_type = "t3a.medium"
+#   key_name        = aws_key_pair.stormfirefox1.key_name
+#   security_groups = [aws_security_group.allow_https_ssh.name]
+#   tags {
+#     Name = "Minecraft Server"
+#   }
+# }
+
+# ON STANBY ON HEROKU, WIP
+# resource "aws_instance" "membership-portal" {
+#   ami           = "ami-0a63cd87767e10ed4"
+#   instance_type = "t3a.micro"
+#   tags {
+#     Name = "Membership Portal API"
+#   }
+# }
+#
+# resource "aws_db_instance" "membership-portal-db" {
+#   allocated_storage = 10
+#   engine            = "postgres"
+#   instance_class    = "db.t3.micro"
+#   name              = var.dbName
+#   username          = var.dbUser
+#   password          = var.dbPass
+# }
+
+resource "aws_s3_bucket" "acmucsd" {
+  bucket = "acmucsd"
+  acl    = "private"
 }
 
-resource "aws_instance" "minecraft" {
-  ami           = "ami-0cd230f950c3de5d8"
-  instance_type = "t3a.medium"
-  tags {
-    Name = "Minecraft Server"
-  }
-}
-
-resource "aws_instance" "membership-portal" {
-  ami           = "ami-0a63cd87767e10ed4"
-  instance_type = "t3a.micro"
-  tags {
-    Name = "Membership Portal API"
-  }
-}
-
-resource "aws_db_instance" "membership-portal-db" {
-  allocated_storage = 10
-  engine            = "postgres"
-  instance_class    = "db.t3.micro"
-  name              = var.dbName
-  username          = var.dbUser
-  password          = var.dbPass
+resource "aws_s3_bucket" "acmucsd-membership-portal" {
+  bucket = "acmucsd-membership-portal"
+  acl    = "private"
 }
